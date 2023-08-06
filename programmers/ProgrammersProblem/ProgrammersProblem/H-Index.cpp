f8987bda50a4823c9875155eada512c583d05f6f
WP8024 <eco2332@gmail.com>
2023-08-06 23:13:52 +0900

    08.06 Update

--- programmers/ProgrammersProblem/ProgrammersProblem/H-Index.cpp
+++ programmers/ProgrammersProblem/ProgrammersProblem/H-Index.cpp
@@ -1,69 +1,27 @@
 #include <string>
 #include <vector>
-#include <stack>
 #include <algorithm>
+#include <stack>
+
 using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-    int h = 0;
-    sort(citations.begin(), citations.end());
-    int start = 0, end = citations.size() - 1;
-    while (start != end) {
-
-        if (citations.size() - end > h) {
-            end--;
-            h = citations[end];
-        }
-        else if (citations[start] < h) {
-            start++;
-        }
-        if (citations.size() - end >= h && citations[start] <= h) {
-            answer = h;
-            return answer;
-        }
-    }
-
-    return answer;
-}
-
-int solution1(vector<int> citations) {
-    int answer = 0;
-    stable_sort(citations.begin(), citations.end(), [](int a, int b)->bool {
+    stack<int> st;
+    sort(citations.begin(), citations.end(), [](int a, int b)->bool {
         return a > b;
     });
-    stack<int> st;
     int h = 0;
-    int n = citations.size();
-    for (auto index : citations) {
-        if (st.empty()) {
-            st.push(index);
-            h = index;
-        }
-        else {
-            if (st.size() < h) {
-                st.push(index);
-                h = index;
-            }
-        }
-    }
-    while (!st.empty()) {
-        if (st.top() > h && st.size() > h) {
-            h++;
-        }
-        else if (st.top() == h || st.size() == h) {
+    for (auto i : citations) {
+        st.push(i);
+        h = st.size();
+        if (st.top() < h) {
             st.pop();
-        }
-        else {
-            answer = h;
+            h = st.size();
             break;
         }
     }
+    answer = h;
 
     return answer;
-}
-
-int main() {
-    solution1({ 3,0,6,1,5 });
-    return 0;
 }
