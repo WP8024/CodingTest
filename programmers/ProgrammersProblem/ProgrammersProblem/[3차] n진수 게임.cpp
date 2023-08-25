664e01b9555d5a9d0c6fffff47ae2e706793f44e
WP8024 <37317856+WP8024@users.noreply.github.com>
2023-08-25 18:34:04 +0900

    08.25 Update

--- programmers/ProgrammersProblem/ProgrammersProblem/[3차] n진수 게임.cpp
+++ programmers/ProgrammersProblem/ProgrammersProblem/[3차] n진수 게임.cpp
@@ -1,31 +1,41 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
-//n은 진법 num%n
-//구할 t갯수 string size
-//참가인원 m
-//튜브 순서p
-//필요한 전체 문자열 데이터= t*m+p
+
 string solution(int n, int t, int m, int p) {
     string answer = "";
     vector<char> numlist = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
 
-    int index = 1, count = 1, num = 0;
+    int c_num = 0;
+    int count = 0;
+    string str = "";
 
-    if (p == 1) {
-        answer += '0';
-    }
     while (answer.size() < t) {
-        num = index;
-        while (num != 0) {
-            count++;
-            if (count % m == p || (m == p && count % m == 0)) {
-                answer += numlist[num % n];
-            }
+        int num = c_num;
+
+        while (num > 0) {
+            str += numlist[num % n];
             num /= n;
         }
-        index++;
+
+        if (str.empty()) {
+            str = '0';
+        }
+        else {
+            reverse(str.begin(), str.end());
+        }
+
+        for (auto ch : str) {
+            count++;
+            if (count % m == p % m) {
+                answer += ch;
+            }
+        }
+        str.clear();
+        c_num++;
     }
+
+    answer = answer.substr(0, t);
     return answer;
 }
